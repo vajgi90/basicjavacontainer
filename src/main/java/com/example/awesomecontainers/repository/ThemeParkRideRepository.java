863a37fd226135ef6302edc4fcf3fb4d3e454d63
vajgi90 <vajgi90@gmail.com>
2023-03-16 19:12:46 +0100

    initial commit

---
+++ src/main/java/com/example/awesomecontainers/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package com.example.awesomecontainers.repository;
+
+import com.example.awesomecontainers.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
---
+++ src/test/java/com/example/awesomecontainers/AwesomecontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awesomecontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomecontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
